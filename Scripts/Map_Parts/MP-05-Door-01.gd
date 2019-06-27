e51deb3ecb5f426bb9d8d61cd66ebc9e59510eb7
David Thornton <Dave@revthornton.com>
2019-06-27 16:35:12 -0700

    fixed and added some door stuff also made a new level 1
    and started on level 2 and added some jail bars and what not super formal talking things end of day 6-27-19

--- Scripts/Map_Parts/MP-05-Door-01.gd
+++ Scripts/Map_Parts/MP-05-Door-01.gd
@@ -5,12 +5,16 @@ export var open_on_left = true
 export var open_on_right = true 
 export var open_on_far_left = false 
 export var open_on_far_right = false 
+export var open_for_bg = true
+export var open_for_player = true
 
 onready var anim = $AnimationPlayer
 onready var timer = $Timer
 onready var far_left = $"Area2D-Left/CollisionShape2D-Left-far"
 onready var far_right = $"Area2D-Right/CollisionShape2D-Right-far"
 onready var sfx = $AudioStreamPlayer
+onready var area_left = $"Area2D-Left"
+onready var area_right = $"Area2D-Right"
 
 var bodies = 0
 var open = false 
@@ -21,7 +25,12 @@ func _ready():
 	timer.wait_time = open_timer
 	far_left.disabled = !open_on_far_left
 	far_right.disabled = !open_on_far_right
-
+	area_left.set_collision_mask_bit(1,open_for_player)
+	area_right.set_collision_mask_bit(1,open_for_player)
+	area_left.set_collision_mask_bit(10,open_for_player)
+	area_right.set_collision_mask_bit(10,open_for_player)
+	area_left.set_collision_mask_bit(11,open_for_bg)
+	area_right.set_collision_mask_bit(11,open_for_bg)
 #warning-ignore:unused_argument
 func _process(delta):
 	if open != open_last:
@@ -60,20 +69,20 @@ func _on_Timer_timeout():
 
 func _on_Area2DLeft_body_entered(body):
 	if open_on_left:
-		if body.get_groups().has("player") || body.get_groups().has("badguy"):
-			_on_entered(body)
+#		if body.get_groups().has("player") || body.get_groups().has("badguy"):
+		_on_entered(body)
 
 func _on_Area2DRight_body_entered(body):
 	if open_on_right:
-		if body.get_groups().has("player") || body.get_groups().has("badguy"):
-			_on_entered(body)
+#		if body.get_groups().has("player") || body.get_groups().has("badguy"):
+		_on_entered(body)
 
 func _on_Area2DLeft_body_exited(body):
 	if open_on_left:
-		if body.get_groups().has("player") || body.get_groups().has("badguy"):
-			_on_exited(body)
+#		if body.get_groups().has("player") || body.get_groups().has("badguy"):
+		_on_exited(body)
 
 func _on_Area2DRight_body_exited(body):
 	if open_on_right:
-		if body.get_groups().has("player") || body.get_groups().has("badguy"):
-			_on_exited(body)
+#		if body.get_groups().has("player") || body.get_groups().has("badguy"):
+		_on_exited(body)
