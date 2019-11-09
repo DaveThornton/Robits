99ec4bb6350f87baf741f56063fee8c0b57383d9
dave <dave@revthornton.com>
2019-11-09 15:47:35 -0800

    Mainly just screen shot and logo type things
     i use for MySpace lol!

--- Scripts/Misc/Singleton_Settings.gd
+++ Scripts/Misc/Singleton_Settings.gd
@@ -3,7 +3,7 @@ extends Node
 #var vol_master = 100
 #var vol_music = 100
 #var vol_sfx = 100
-var max_num_of_player = 4
+var max_num_of_player = 8
 var end_game_score = 10
 # Called when the node enters the scene tree for the first time.
 
