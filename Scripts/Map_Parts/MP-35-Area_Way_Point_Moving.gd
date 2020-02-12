ecc0b70a69b4ec468027afc765dd93063af442bd
dave <dave@revthornton.com>
2020-02-11 16:09:42 -0800

    map part 35 added anything follow way point

--- Scripts/Map_Parts/MP-35-Area_Way_Point_Moving.gd
+++ Scripts/Map_Parts/MP-35-Area_Way_Point_Moving.gd
@@ -1,10 +1,8 @@
 tool
-extends KinematicBody2D
+extends Area2D
 
 onready var timer = $Timer
 onready var waypoints: = get_node(waypoints_path)
-onready var move_area: = $Area2D
-onready var player_area_shape = $Area2D/CollisionShape2D
 
 export var editor_process: = true setget set_editor_process
 export var speed: = 400.0
@@ -14,13 +12,11 @@ export var waypoints_path: = NodePath()
 var target_position = Vector2()
 var _robits: Array = []
 
-
-func _ready() -> void:
+func _ready():
 	if not waypoints:
 		set_physics_process(false)
 		return
 	target_position = waypoints.get_start_position()
-	player_area_shape = player_area_size
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = (target_position - position).normalized()
@@ -36,27 +32,24 @@ func _physics_process(delta: float) -> void:
 		if _robits.size() > 0:
 			for i in _robits.size():
 				_robits[i].position += motion
-			print("fix way point MP-37")
+			print("fix way point MP-35")
 
 func set_editor_process(value:bool):
 	editor_process = value
 	if not Engine.editor_hint:
 		return
 	set_physics_process(value)
-#	timer.stop()
 
-#func _process(delta):
-#	pass
+func _on_MP35Area_Way_Point_Moving_body_entered(body):
+	if body.get_groups().has("player"):
+		print("player entered platform map part 35")
+		_robits.append(body)
+
+func _on_MP35Area_Way_Point_Moving_body_exited(body):
+	if body.get_groups().has("player"):
+		print("player exited platform map part 35")
+		_robits.erase(body)
+
 
 func _on_Timer_timeout():
 	set_physics_process(true)
-
-func _on_Area2D_body_entered(body):
-	if body.get_groups().has("player"):
-		print("player entered platform map part 37")
-		_robits.append(body)
-
-func _on_Area2D_body_exited(body):
-	if body.get_groups().has("player"):
-		print("player exited platform map part 37")
-		_robits.erase(body)
