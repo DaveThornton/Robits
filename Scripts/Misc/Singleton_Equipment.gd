08a3df2f9505147d9b96c4da84ae5dcdba4ea989
dave <dave@revthornton.com>
2020-11-12 23:55:45 -0800

    Added Solo Blaster

--- Scripts/Misc/Singleton_Equipment.gd
+++ Scripts/Misc/Singleton_Equipment.gd
@@ -16,7 +16,7 @@ export(PackedScene) var gun_hold_12
 export(PackedScene) var gun_hold_13
 #export(PackedScene) var gun_hold_14
 export(PackedScene) var gun_hold_15
-#export(PackedScene) var gun_hold_16
+export(PackedScene) var gun_hold_16
 #export(PackedScene) var gun_hold_17
 #export(PackedScene) var gun_hold_18
 #export(PackedScene) var gun_hold_19
@@ -163,8 +163,8 @@ func get_weap_hold(_weap: int):
 #			return gun_hold_14
 		elif _weap == 15:
 			return gun_hold_15
-#		elif _weap == 16:
-#			return gun_hold_16
+		elif _weap == 16:
+			return gun_hold_16
 #		elif _weap == 17:
 #			return gun_hold_17
 #		elif _weap == 18:
