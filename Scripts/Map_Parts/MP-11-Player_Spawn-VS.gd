489dc1cadb134582d961d39467fd6896ec0ccb8e
dave <dave@revthornton.com>
2020-12-17 16:11:12 -0800

    Made spawns into random spots
    it used to be in order

---
+++ Scripts/Map_Parts/MP-11-Player_Spawn-VS.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+onready var sprite = $Sprite
+
+var spawnable = true
+var count = 0 
+
+func _ready():
+	sprite.visible = false
+
+func can_spawn():
+	return spawnable
+
+func set_spawn(_spawn):
+	spawnable = _spawn
+
+func _on_Area2D_body_entered(body):
+	if body.get_groups().has("player"):
+		if Map_Hand.map:
+			Map_Hand.map.remove_spawn_pos(self)
+		spawnable = false 
+		count += 1
+#	print("entered    ",spawnable)
+
+func _on_Area2D_body_exited(body):
+	if body.get_groups().has("player"):
+		count -= 1
+		if count <= 0:
+			if Map_Hand.map:
+				Map_Hand.map.add_spawn_pos(self)
+			spawnable = true
+#			count = 0
+#	print("exited    ",spawnable)
--- map/map-00.gd
+++ map/map-00.gd
@@ -7,11 +7,14 @@ export var camera_move = false
 export var camera_max_right = 1920
 export var background = 1
 onready var player_spawns = $Player_spawns
-onready var parts = $Map_parts
+onready var parts = $Map_parts 
 var next_spawn_spot = 0
+var spawn_spots = []
 var nav_system
-
+var first = true
+var num_in_play = 0
 func _ready():
+
 #	Game.mode = 2
 	Game.use_lives() #checks if number of lives are used
 	if $"MP-04-Nav2D":
@@ -26,10 +29,48 @@ func _ready():
 		HUD.splash(top_text, body_text, 1.5, true)
 
 func next_spawn_pos():
-	next_spawn_spot += 1
-	if next_spawn_spot > player_spawns.get_child_count() - 1:
-		next_spawn_spot = 0
-	return player_spawns.get_child(next_spawn_spot).position
+	if first:
+		for i in player_spawns.get_child_count():
+			spawn_spots.append(player_spawns.get_child(i))
+		first = false
+	spawn_spots.shuffle()
+#	var spot = spawn_spots.pop_front()
+	return spawn_spots.pop_front().position
+#	if first:
+#		num_in_play += 1
+#		next_spawn_spot += 1
+#		if num_in_play >= Player_Stats.get_num_in_game():
+#			first = false
+#		print(num_in_play,"   ",next_spawn_spot, "   ",first)
+#		return player_spawns.get_child(next_spawn_spot).position
+#
+#	for i in player_spawns.get_child_count():
+#		if player_spawns.get_child(i).can_spawn():
+##			print("next spawn spot should be true  = ",player_spawns.get_child(i).can_spawn())
+#			spawn_spots.append(player_spawns.get_child(i))
+
+#	next_spawn_spot += 1
+#	if spawn_spots.size() <= next_spawn_spot:
+#		next_spawn_spot = 0 
+	
+#	var this_pos = randi() % (spawn_spots.size())
+#	print(this_pos)
+#	spawn_spots[this_pos].set_spawn(false)
+#	return spawn_spots[this_pos].position
+#	return spawn_spots[int(rand_range(0,spawn_spots.size()))].position
+
+#	if next_spawn_spot > player_spawns.get_child_count() - 1:
+#		next_spawn_spot = 0
+#	return player_spawns.get_child(next_spawn_spot).position
+
+func remove_spawn_pos(_pos):
+	if spawn_spots.has(_pos):
+		spawn_spots.erase(_pos)
+
+func add_spawn_pos(_pos):
+	if !spawn_spots.has(_pos):
+		spawn_spots.append(_pos)
+
 
 func remove_map():
 	reset()
