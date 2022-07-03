a41498a78ab7b214cc46aa15e3ad12606c4fd139
David Thornton <Dave@revthornton.com>
2022-07-02 23:15:22 -0700

    fixed the broken music from last update
    should have tested it first oof!

--- Scripts/Misc/Parts/Music_Part.gd
+++ Scripts/Misc/Parts/Music_Part.gd
@@ -7,7 +7,7 @@ onready var menu = $Menu
 var song_playing
 
 func _ready():
-	song_playing = self.get_child(0)
+	song_playing = level.get_child(0)
 
 func play(_num):
 	stop()
@@ -18,12 +18,12 @@ func play(_num):
 		else: print_debug("error invalid song number: ", _num, "   in music part   1")
 	elif _num >= 900:
 		if (_num - 900) <= menu.get_child_count():
-			song_playing = menu.get_child(_num)
+			song_playing = menu.get_child(_num - 900)
 			song_playing.play()
 		else: print_debug("error invalid song number: ", _num, "   in music part   2")
 	elif _num >= 700:
 		if (_num - 700) <= boss.get_child_count():
-			song_playing = boss.get_child(_num)
+			song_playing = boss.get_child(_num - 700)
 			song_playing.play()
 		else: print_debug("error invalid song number: ", _num, "   in music part   3")
 
