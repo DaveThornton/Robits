89653527d4cae24df3eb6b8510e60be5288c53b0
David Thornton <Dave@revthornton.com>
2023-03-12 00:10:09 -0800

    Rail Gun on new weapon system
    have to start on the grenades next

--- Scripts/Guns/Hold/W_19-Rail_Gun_Hold.gd
+++ Scripts/Guns/Hold/W_19-Rail_Gun_Hold.gd
@@ -1,60 +1,12 @@
-extends Node2D
+extends 'res://Scripts/Guns/Hold/W_00_Gun_Hold.gd'
 
-onready var anim_fire = $AnimationPlayer
-onready var melee_timer = $Melee_Timer
-onready var shoot_timer = $Shoot_Timer
-onready var shoot_cast = $POS_Gun/Raycast/Shoot
-onready var melee_cast = $POS_Gun/Raycast/Melee
-onready var throw_cast = $POS_Gun/Raycast/Throw
-onready var pos_shoot = $POS_Gun/POS/Shoot
-onready var pos_shell = $POS_Gun/POS/Shell
-onready var pos_throw = $POS_Gun/POS/Throw
 onready var beam = $POS_Gun/Gun_Sprite/Beam
 onready var beam_end =$POS_Gun/Gun_Sprite/Beam_End
 onready var partic = $POS_Gun/Gun_Sprite/CPUParticles2D
-onready var melee_area = $POS_Gun/Melee_Area
 
-var player = 1
-var pawn = 0
-var gun_num = 19
-var ammo = 30
-var ammo_max = 90
-var take_ammo = true
-var my_name = "Rail Gun"
-var new_anim = "Un_pos"
-var old_anim = "Un_pos"
-var dmg_type = "Bullet"
-var damage = 100
-var can_shoot = true
-var just_shot = false
-var shoot_pos = 3
-var walk = 0
-var change_shoot_pos = true
-var is_right = true
-var time = 4.0
 var max_beam = 2000
 var shoot_pressed = false
 var shoot_pressed_time = 0
-
-signal ammo_change(player, ammo)
-
-func _ready():
-	var test1 = self.connect("ammo_change", Player_Stats, "ammo_update")
-	if test1 != 0:
-		print_debug("failed to connect ammo change in weap hold 19 rail gun")
-	if Game.get_mode() == 0:
-		shoot_cast.set_collision_mask(FX.projectiles.get_layer_mode_0_b())
-		melee_cast.set_collision_mask(FX.projectiles.get_layer_mode_0_b())
-
-func init(_ammo, _player, _timer, _just_shot):
-	ammo = _ammo
-	player = _player
-	shoot_cast.set_collision_mask_bit(Player_Stats.get_player_collision_layer(_player) - 1, false)
-	melee_cast.set_collision_mask_bit(Player_Stats.get_player_collision_layer(_player) - 1, false)
-	throw_cast.set_collision_mask_bit(Player_Stats.get_player_collision_layer(_player) - 1, false)
-	melee_area.set_collision_mask_bit(Player_Stats.get_player_collision_layer(_player) - 1, false)
-	just_shot = _just_shot
-	emit_signal("ammo_change",player,ammo)
 
 func _physics_process(delta):
 	if shoot_cast.is_colliding():
@@ -64,8 +16,6 @@ func _physics_process(delta):
 	beam.region_rect.end.x = beam_end.position.length()
 	if shoot_pressed && can_shoot:
 		shoot_pressed_time += delta
-	# if can_shoot && !beam.visible:
-	# 	beam.visible = true
 
 func shoot_j():
 	shoot_pressed = true
@@ -81,7 +31,7 @@ func shoot():
 				if !shoot_cast.is_colliding():
 					print_debug("shoot cast not colliding why gun 19")
 				else: 
-					FX.explode(19, player, shoot_cast.get_collision_point(), gun_num, pawn, damage)
+					FX.explode(19, player, shoot_cast.get_collision_point(), gun_num, 0, damage)
 				anim_fire.play("Shoot")
 				ammo = clamp(ammo - 1, 0, ammo_max)
 				emit_signal("ammo_change",player,ammo)
@@ -99,84 +49,3 @@ func shoot_r():
 	SFX.stop("W_19_Charge_Up")
 	shoot_pressed = false
 	shoot_pressed_time = 0 
-
-func melee():
-	if can_shoot:
-		can_shoot = false
-		anim_fire.play("Melee")
-		melee_timer.start()
-		Player_Stats.add_shot(player, 1)
-
-func _on_Melee_Area_body_entered(body):
-	if body.get_groups().has("player"):
-		if body.player != player:
-			body.hit(player, gun_num, dmg_type, damage)
-		else:
-			print_debug("quit hitting your self")
-
-func throw():
-	var t = Equipment.get_weap_pick(gun_num).instance()
-	Map_Hand.add_kid_to_map(t)
-	t.init(ammo, player, 1, is_right, shoot_pos, false)
-	if throw_cast.is_colliding():
-		t.position = self.global_position
-		_drop_where(t)
-	else:
-		t.position = pos_throw.global_position
-		_throw_where(t)
-	emit_signal("ammo_change",player,0)
-	call_deferred("free")
-
-func drop():
-	call_deferred("_drop")
-func _drop():
-	var t = Equipment.get_weap_pick(gun_num).instance()
-	Map_Hand.add_kid_to_map(t)
-	t.position = pos_throw.global_position
-	t.init(ammo, player, 1, is_right, shoot_pos, false)
-	_drop_where(t)
-	emit_signal("ammo_change",player,0)
-	call_deferred("free")
-
-func set_shoot_pos(_num, _is_right):
-	if change_shoot_pos:
-		shoot_pos = _num
-		is_right = _is_right
-
-func add_ammo(_ammo):
-	ammo = clamp(ammo + _ammo, 0, ammo_max)
-	emit_signal("ammo_change",player,ammo)
-
-func _throw_where(_obj):
-	if is_right:
-		if shoot_pos == 1:
-			_obj.apply_impulse(pos_throw.position, Vector2(100, -700))
-		elif shoot_pos == 2:
-			_obj.apply_impulse(pos_throw.position, Vector2(600, -400))
-		elif shoot_pos == 3 || shoot_pos == 6:
-			_obj.apply_impulse(pos_throw.position, Vector2(600, -200))
-		elif shoot_pos == 4:
-			_obj.apply_impulse(pos_throw.position, Vector2(600, 200))
-		elif shoot_pos == 5:
-			_obj.apply_impulse(pos_throw.position, Vector2(100, 700))
-	else:
-		if shoot_pos == 1:
-			_obj.apply_impulse(pos_throw.position, Vector2(-100, -700))
-		elif shoot_pos == 2:
-			_obj.apply_impulse(pos_throw.position, Vector2(-600, -400))
-		elif shoot_pos == 3 ||shoot_pos == 6:
-			_obj.apply_impulse(pos_throw.position, Vector2(-600, -200))
-		elif shoot_pos == 4:
-			_obj.apply_impulse(pos_throw.position, Vector2(-600, 200))
-		elif shoot_pos == 5:
-			_obj.apply_impulse(pos_throw.position, Vector2(-100, 700))
-
-func _drop_where(_obj):
-	_obj.set_collision_layer_bit( 1, false)
-	_obj.set_collision_mask_bit( 1, false)
-
-func _on_Shoot_Timer_timeout():
-	can_shoot = true
-
-func _on_Melee_Timer_timeout():
-	can_shoot = true
