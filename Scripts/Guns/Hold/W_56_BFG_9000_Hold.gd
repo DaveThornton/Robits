aed63c2abcaa1b43fe8e72bb5129eaa5003d9d4e
David Thornton <Dave@revthornton.com>
2023-03-15 10:17:14 -0700

    Brought 57 in to new system
    also updated the art for the same gun 57 henry

--- Scripts/Guns/Hold/W_56_BFG_9000_Hold.gd
+++ Scripts/Guns/Hold/W_56_BFG_9000_Hold.gd
@@ -11,38 +11,6 @@ func post_set_up():
 
 func shoot_j():
 	_fire()
-	# if can_shoot:
-	# 	print_debug("i can shoot so whats the problem")
-	# 	if melee_cast.is_colliding():
-	# 		if melee_cast.get_collider().get_groups().has("player"):
-	# 			if melee_cast.get_collider().player == player:
-	# 				fire_projectile()
-	# 			else:
-	# 				melee()
-	# 		else:
-	# 			melee()
-	# 	elif ammo > 0:
-	# 		fire_projectile()
-
-# func fire_projectile():
-# 	if !shoot_cast.is_colliding():
-# 		var _ss = pos_shoot.global_position
-# 		var _sr = pos_shoot.global_rotation
-# 		if is_right:
-# 			_sr = pos_shoot.global_rotation
-# 		else:
-# 			_sr = pos_shoot.global_rotation * -1
-# 		var _sss = pos_shoot.global_scale
-# 		FX.proj(gun_num, _sr, _ss, _sss, player, damage)
-# 	else:
-# 		FX.explode(56.1, player, shoot_cast.get_collision_point(), gun_num, 0, damage)
-# 	SFX.play("W_56_Shoot")
-# 	ammo = clamp(ammo - 1, 0, ammo_max)
-# 	sprite_gun.frame = 1
-# 	emit_signal("ammo_change",player,ammo)
-# 	Player_Stats.add_shot(player, 1)
-# 	can_shoot = false
-# 	shoot_timer.start()
 
 func _fire_no_projectile():
 	FX.explode(56.1, player, shoot_cast.get_collision_point(), gun_num, 0, damage)
