c5297b35885680ad1f06708f5e29232a937f97fb
David Thornton <Dave@revthornton.com>
2023-03-30 02:19:46 -0700

    AK-47 mag now disappears  if it is out of ammo

--- Scripts/Guns/Hold/W_02-AK47_Hold.gd
+++ Scripts/Guns/Hold/W_02-AK47_Hold.gd
@@ -1,4 +1,12 @@
 extends 'res://Scripts/Guns/Hold/W_00_Gun_Hold.gd'
 
+onready var sprite2 = $POS_Gun/Gun_Sprite/Gun_Sprite2
+
 func shoot():
 	_fire()
+
+func call_on_all_projectile_fire():
+	if ammo == 0:
+		sprite2.visible = false
+	else:
+		sprite2.visible = true
--- Scripts/Guns/Pick/W_02_AK_47_Pick.gd
+++ Scripts/Guns/Pick/W_02_AK_47_Pick.gd
@@ -1,10 +1,18 @@
 extends 'res://Scripts/Guns/Pick/W_00_Gun_Pick.gd'
 
+onready var sprite2 = $Sprite/Sprite2
+
+func call_end_init():
+	if ammo == 0:
+		sprite2.visible = false
+
 func _on_WeapPick02Ak47_body_shape_entered(_body_id, body, _body_shape, _local_shape):
 	_entered(_body_id, body, _body_shape, _local_shape)
 	
 func set_right():
-	sprite.scale.y = 1.5
+	sprite.scale.y = 1
+	sprite2.scale.y = 1
 
 func set_left():
-	sprite.scale.y = -1.5
+	sprite.scale.y = -1
+	sprite2.scale.y = -1
