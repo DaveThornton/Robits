e58eb1ed9e5d4f6aee3db33c58815162e6328974
David Thornton <Dave@revthornton.com>
2023-03-30 12:28:34 -0700

    Changed the size of the AK and also fixed a little bug it had

--- Scripts/Guns/Hold/W_02-AK47_Hold.gd
+++ Scripts/Guns/Hold/W_02-AK47_Hold.gd
@@ -5,7 +5,13 @@ onready var sprite2 = $POS_Gun/Gun_Sprite/Gun_Sprite2
 func shoot():
 	_fire()
 
+func post_set_up():
+	check_if_zero()
+
 func call_on_all_projectile_fire():
+	check_if_zero()
+
+func check_if_zero():
 	if ammo == 0:
 		sprite2.visible = false
 	else:
--- Scripts/Guns/Pick/W_02_AK_47_Pick.gd
+++ Scripts/Guns/Pick/W_02_AK_47_Pick.gd
@@ -10,9 +10,13 @@ func _on_WeapPick02Ak47_body_shape_entered(_body_id, body, _body_shape, _local_s
 	_entered(_body_id, body, _body_shape, _local_shape)
 	
 func set_right():
+	call_deferred("deferred_right")
+func deferred_right():
 	sprite.scale.y = 1
 	sprite2.scale.y = 1
 
 func set_left():
+	call_deferred("deferred_left")
+func deferred_left():
 	sprite.scale.y = -1
 	sprite2.scale.y = -1
