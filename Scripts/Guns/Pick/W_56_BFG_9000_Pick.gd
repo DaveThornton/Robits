028cbd474b714fe832ac5d8040aec5806b425c4a
David Thornton <Dave@revthornton.com>
2023-04-01 10:14:13 -0700

    Freshened up the BFG 9000 Art and Animation

--- Scripts/Guns/Pick/W_56_BFG_9000_Pick.gd
+++ Scripts/Guns/Pick/W_56_BFG_9000_Pick.gd
@@ -1,5 +1,10 @@
 extends 'res://Scripts/Guns/Pick/W_00_Gun_Pick.gd'
 
+onready var anim = $AnimationPlayer
+
+func call_end_init():
+	anim.play("Idle")
+
 func _on_WeapPick51Contradiction_S_body_shape_entered(_body_id, _body, _body_shape, _local_shape):
 	_entered(_body_id, _body, _body_shape, _local_shape)
 
@@ -8,3 +13,6 @@ func set_right():
 
 func set_left():
 	sprite.scale.y = -1.5
+
+func ammo_is_zero():
+	anim.play("Stop")
