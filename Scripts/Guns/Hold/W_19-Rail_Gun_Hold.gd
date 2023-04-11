60aace6ab267c0b758e0044d3b78ca3292b393e7
David Thornton <Dave@revthornton.com>
2023-04-11 10:10:55 -0700

    New graphics for W19 Rail Gun  and EX 19
    also EX 19 now on New system

--- Scripts/Guns/Hold/W_19-Rail_Gun_Hold.gd
+++ Scripts/Guns/Hold/W_19-Rail_Gun_Hold.gd
@@ -4,9 +4,20 @@ onready var beam = $POS_Gun/Gun_Sprite/Beam
 onready var beam_end =$POS_Gun/Gun_Sprite/Beam_End
 onready var partic = $POS_Gun/Gun_Sprite/CPUParticles2D
 
+onready var ammo_01 = $POS_Gun/Gun_Sprite/Ammo_01
+onready var ammo_02 = $POS_Gun/Gun_Sprite/Ammo_02
+onready var ammo_03 = $POS_Gun/Gun_Sprite/Ammo_03
+onready var ammo_04 = $POS_Gun/Gun_Sprite/Ammo_04
+onready var ammo_05 = $POS_Gun/Gun_Sprite/Ammo_05
+
+export var ex_num = 19
+
 var max_beam = 2000
 var shoot_pressed = false
 var shoot_pressed_time = 0
+
+func post_set_up():
+	ammo_update()
 
 func _physics_process(delta):
 	if shoot_cast.is_colliding():
@@ -18,9 +29,10 @@ func _physics_process(delta):
 		shoot_pressed_time += delta
 
 func shoot_j():
-	shoot_pressed = true
-	anim_fire.play("ChargeUp")
-	SFX.play("W_19_Charge_Up")
+	if ammo > 0:
+		shoot_pressed = true
+		anim_fire.play("ChargeUp")
+		SFX.play("W_19_Charge_Up")
 
 func shoot():
 	if shoot_pressed_time > 1 && shoot_pressed:
@@ -31,10 +43,11 @@ func shoot():
 				if !shoot_cast.is_colliding():
 					print_debug("shoot cast not colliding why gun 19")
 				else: 
-					FX.explode(19, player, shoot_cast.get_collision_point(), gun_num, 0, damage)
+					FX.explode(ex_num, player, shoot_cast.get_collision_point(), gun_num, 0, damage)
 				anim_fire.play("Shoot")
 				ammo = clamp(ammo - 1, 0, ammo_max)
 				emit_signal("ammo_change",player,ammo)
+				ammo_update()
 				Player_Stats.add_shot(player, 1)
 				just_shot = true
 				SFX.projectile(gun_num)
@@ -49,3 +62,30 @@ func shoot_r():
 	SFX.stop("W_19_Charge_Up")
 	shoot_pressed = false
 	shoot_pressed_time = 0 
+
+func ammo_update():
+	ammo_01.visible = false
+	ammo_02.visible = false
+	ammo_03.visible = false
+	ammo_04.visible = false
+	ammo_05.visible = false
+	if ammo >= 5:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+		ammo_04.visible = true
+		ammo_05.visible = true
+	elif ammo >= 4:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+		ammo_04.visible = true
+	elif ammo >= 3:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+	elif ammo >= 2:
+		ammo_01.visible = true
+		ammo_02.visible = true
+	elif ammo >= 1:
+		ammo_01.visible = true
--- Scripts/Guns/Pick/W_19-Rail_Gun_Pick.gd
+++ Scripts/Guns/Pick/W_19-Rail_Gun_Pick.gd
@@ -1,10 +1,46 @@
 extends 'res://Scripts/Guns/Pick/W_00_Gun_Pick.gd'
 
+onready var ammo_01 = $Sprite/Ammo_01
+onready var ammo_02 = $Sprite/Ammo_02
+onready var ammo_03 = $Sprite/Ammo_03
+onready var ammo_04 = $Sprite/Ammo_04
+onready var ammo_05 = $Sprite/Ammo_05
+
+func call_end_init():
+	ammo_update()
+
 func _on_WeapPick03Sniper_body_shape_entered(_body_id, body, _body_shape, _local_shape):
 	_entered(_body_id, body, _body_shape, _local_shape)
 
 func set_right():
-	sprite.scale.y = 1.5
+	sprite.scale.y = 1
 
 func set_left():
-	sprite.scale.y = -1.5
+	sprite.scale.y = -1
+
+func ammo_update():
+	ammo_01.visible = false
+	ammo_02.visible = false
+	ammo_03.visible = false
+	ammo_04.visible = false
+	ammo_05.visible = false
+	if ammo >= 5:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+		ammo_04.visible = true
+		ammo_05.visible = true
+	elif ammo >= 4:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+		ammo_04.visible = true
+	elif ammo >= 3:
+		ammo_01.visible = true
+		ammo_02.visible = true
+		ammo_03.visible = true
+	elif ammo >= 2:
+		ammo_01.visible = true
+		ammo_02.visible = true
+	elif ammo >= 1:
+		ammo_01.visible = true
