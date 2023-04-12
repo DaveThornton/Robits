f77e809a0a1399ea3e95a3fe5bd87674a2326682
David Thornton <Dave@revthornton.com>
2023-04-12 14:11:18 -0700

    Improved Outline System
    also a bunch of outlines on weapons

--- Scripts/Guns/Pick/W_07_CrossBow_Pick.gd
+++ Scripts/Guns/Pick/W_07_CrossBow_Pick.gd
@@ -1,16 +1,17 @@
 extends 'res://Scripts/Guns/Pick/W_00_Gun_Pick.gd'
 
 onready var sprite2 = $Sprite/Arrow
-
+onready var outline = $Sprite/Outline
 func _on_W_07_CrossBow_Pick_body_shape_entered(_body_id, _body, _body_shape, _local_shape):
 	_entered(_body_id, _body, _body_shape, _local_shape)
 
 func set_right():
-	sprite.scale.y = 1.5
+	sprite.scale.y = 1
 
 func set_left():
-	sprite.scale.y = -1.5
+	sprite.scale.y = -1
 
 func ammo_is_zero():
 	sprite.frame = 2
 	sprite2.visible = false
+	outline.frame = 2
---
+++ Scripts/Misc/Parts/Outline_Pick.gd
@@ -0,0 +1,3 @@
+extends Sprite
+
+func _ready(): self_modulate = FX.get_outline_color()
