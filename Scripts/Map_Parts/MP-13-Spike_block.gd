1a1c90114dd699db41b9ace16b75b4189caa9ad3
David Thornton <Dave@revthornton.com>
2023-04-30 00:14:54 -0700

    Updated the Spike block for new system

--- Scripts/Map_Parts/MP-13-Spike_block.gd
+++ Scripts/Map_Parts/MP-13-Spike_block.gd
@@ -1,12 +1,7 @@
 extends Node2D
 
-#export var is_visible = true 
-export var auto_kill = true
 export var my_name = "spike"
 
 func _on_Area2D_body_entered(body):
 	if body.get_groups().has("player"):
-		if auto_kill:
-			body.killed_by_map(0, my_name,my_name, 50)
-		else:
-			body.hit(0, my_name, my_name, 50)
+		body.killed_by_map(0, my_name,my_name, 50)
