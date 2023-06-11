6c2338b13f607edd83407633b67a4386be2faa97
David Thornton <Dave@revthornton.com>
2023-06-10 21:32:13 -0700

    Better Dirt texture replace all of them I Think

--- Scripts/Guns/Pick/W_20_Grenade_Pick.gd
+++ Scripts/Guns/Pick/W_20_Grenade_Pick.gd
@@ -1,5 +1,8 @@
 extends 'res://Scripts/Guns/Pick/W_00_Bomb_Pick.gd'
 
+func _ready():
+	time = my_time
+	outline = $POS_Gun/Outline
+
 func set_outline_color(_color):
-    outline = $POS_Gun/Outline
     outline.self_modulate = _color
