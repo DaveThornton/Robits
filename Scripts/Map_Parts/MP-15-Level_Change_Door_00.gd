4f981faa82bfa3e48220015630b4eb1b2e6580e9
David Thornton <Dave@revthornton.com>
2023-07-02 23:59:55 -0700

    fixed a bunch of errors in the level change door MP 15

--- Scripts/Map_Parts/MP-15-Level_Change_Door_00.gd
+++ Scripts/Map_Parts/MP-15-Level_Change_Door_00.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var anim = $AnimationPlayer
+onready var timer = $Timer
 
 var map_to_load = 1
 var bodies = []
@@ -12,12 +13,17 @@ func load_map():
 	var map_w_info = Map_Hand.map.get_next_level_w_info (map_to_load)#0: next map number to load, 1: use spash ,  2: splash title , 3: splash body ,  4: lenght of splash in seconds
 	var map_scene_to_load = Campaign.get_map(map_w_info[0])
 	Map_Hand.load_map_cam_first(map_scene_to_load, map_w_info[2], map_w_info[3], map_w_info[4], map_w_info[1])
+	call_deferred("queue_free")
 
 func open():
-	anim.play("Door-Open")
+	if anim.is_active() && anim.get_current_animation() != "Open":
+		anim.queue("Open")
+	elif !anim.is_active():
+		anim.play("Open")
+	print(anim.get_current_animation())
 
 func close():
-	anim.play("Door-Close")
+	anim.play("Close")
 
 func set_open(open: bool):
 	is_open = open
@@ -32,12 +38,12 @@ func _on_Area2D_body_entered(body:Node):
 		else:
 			bodies.append(body)
 			if anim.is_playing():
-				if anim.get_current_animaion() == "Open":
+				if anim.get_current_animation() == "Open":
 					pass
 				else:
-					anim.queue("Open")
+					open()
 			else:
-				anim.play("Open")
+				open()
 
 func _on_Area2D_body_exited(body:Node):
 	if bodies.find(body) >= 0:
@@ -47,9 +53,14 @@ func _on_AnimationPlayer_animation_finished(anim_name:String):
 	if anim_name == "Open":
 		set_open(true)
 		if bodies.size() > 0:
+			print("more than nun ", bodies.size())
 			call_deferred("load_map")
-			# load_map()
+		else:
+			timer.start()
 
 func _on_AnimationPlayer_animation_started(anim_name:String):
 	if anim_name == "Close":
 		set_open(false)
+
+func _on_Timer_timeout():
+	close()
